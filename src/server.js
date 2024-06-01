ff985a819814a014dacc794e0e147c305b25290e
william emanoel silva martins <williamemanoel777@gmail.com>
2024-06-01 12:02:43 -0300

    primeiro envio

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const oracledb = require('oracledb');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 app.use(bodyParser.json());
 
