84c92bcaa81344e44b187b1b609c878b535e61da
william emanoel silva martins <williamemanoel777@gmail.com>
2024-06-15 16:29:18 -0300

    Mensagem descrevendo as

--- src/server.js
+++ src/server.js
@@ -36,7 +36,7 @@ app.get("/", (req, res) => {
 
 // Rota para o INSERT
 app.post('/notasteste2', async (req, res) => {
-  const { valor1, valor2, valor3, valor4 } = req.body;
+  const { valor1, valor2, valor3, valor4, valor5, valor6, valor7, valor8, itens2, itens3 } = req.body;
 
   try {
     console.log('Recebido POST /notasteste2 com dados:', req.body);
@@ -44,12 +44,26 @@ app.post('/notasteste2', async (req, res) => {
     let connection = await oracledb.getConnection(dbConfig);
     console.log('Conexão com o banco de dados estabelecida com sucesso.');
 
-    const sqlInsert = `INSERT INTO NOTASTESTE2 (MODOPAGAMENTO, PARCELAS, DATAFULL, VALOR) VALUES(:valor1, :valor2, :valor3, :valor4)`;
-    let binds = { valor1, valor2, valor3, valor4 };
-    const options = { autoCommit: true, outFormat: oracledb.OUT_FORMAT_OBJECT };
+    // Inicia uma transação
+    await connection.execute('BEGIN');
 
-    await connection.execute(sqlInsert, binds, options);
-    console.log('Inserção realizada com sucesso.');
+    const sqlInsertPedido = `INSERT INTO PEDIDO_PAG_SIMPLES (ID, MODOPAGAMENTO, PARCELAS, DATAFULL, VALOR, CAUT_YA07, TBAND_YA06, CNPJ_YA05) VALUES (:valor1, :valor2, :valor3, :valor4, :valor5, :valor6, :valor7, :valor8)`;
+    let bindsPedido = { valor1, valor2, valor3, valor4, valor5, valor6, valor7, valor8 };
+    const options = { autoCommit: false, outFormat: oracledb.OUT_FORMAT_OBJECT };
+
+    await connection.execute(sqlInsertPedido, bindsPedido, options);
+    console.log('Inserção em PEDIDO_PAG_SIMPLES realizada com sucesso.');
+
+    const sqlInsertItens = `INSERT INTO ITENS (ID, ITEM, QTD) VALUES (:id, itens2 ,itens3)`;
+    for (const item of itens) {
+      let bindsItem = { id: valor1, itens2 ,itens3 };
+      await connection.execute(sqlInsertItens, bindsItem, options);
+    }
+    console.log('Inserção em ITENS realizada com sucesso.');
+
+    // Commit a transação
+    await connection.commit();
+    console.log('Transação commitada com sucesso.');
 
     await connection.close();
     console.log('Conexão com o banco de dados fechada com sucesso.');
@@ -57,9 +71,19 @@ app.post('/notasteste2', async (req, res) => {
     res.status(201).json({ message: 'Inserção realizada com sucesso.' });
   } catch (err) {
     console.error('Erro durante a execução:', err);
+
+    if (connection) {
+      try {
+        await connection.rollback();
+        console.log('Transação revertida com sucesso.');
+      } catch (rollbackErr) {
+        console.error('Erro durante o rollback:', rollbackErr);
+      }
+    }
+
     res.status(500).json({ error: err.message });
   }
-}); 
+});
 
 app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}`);
